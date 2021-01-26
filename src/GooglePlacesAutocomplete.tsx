912c36fade6250f1edda3ddeb0918e72170357a4
Will Turnage <will@turnage.co>
2021-01-26 14:13:16 -0500

    Upgrading to latest versions

--- src/GooglePlacesAutocomplete.tsx
+++ src/GooglePlacesAutocomplete.tsx
@@ -1,48 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import AsyncSelect from 'react-select/async';
-import { OptionsType, OptionTypeBase } from 'react-select';
-import { useDebouncedCallback } from 'use-debounce';
-import GooglePlacesAutocompleteProps, { AutocompletionRequest }from './GooglePlacesAutocomplete.types';
-import injectScript from './helpers/injectScript';
-import autocompletionRequestBuilder from './helpers/autocompletionRequestBuilder';
+import React, { useEffect, useState } from "react";
+import AsyncSelect from "react-select/async";
+import { OptionsType, OptionTypeBase } from "react-select";
+import { useDebouncedCallback } from "use-debounce";
+import GooglePlacesAutocompleteProps, {
+  AutocompletionRequest,
+} from "./GooglePlacesAutocomplete.types";
+import injectScript from "./helpers/injectScript";
+import autocompletionRequestBuilder from "./helpers/autocompletionRequestBuilder";
 
 const GooglePlacesAutocomplete: React.FC<GooglePlacesAutocompleteProps> = ({
-  apiKey = '',
+  apiKey = "",
   autocompletionRequest = {},
   debounce = 300,
   minLengthAutocomplete = 0,
   selectProps = {},
   onLoadFailed = console.error,
   withSessionToken = false,
-} : GooglePlacesAutocompleteProps) : React.ReactElement => {
-  const [placesService, setPlacesService] = useState<google.maps.places.AutocompleteService | undefined>(undefined);
-  const [sessionToken, setSessionToken] = useState<google.maps.places.AutocompleteSessionToken | undefined>(undefined);
-  const [fetchSuggestions] = useDebouncedCallback((value: string, cb: (options: OptionsType<OptionTypeBase>) => void): void => {
-    if (!placesService) return cb([]);
-    if (value.length < minLengthAutocomplete) return cb([]);
+}: GooglePlacesAutocompleteProps): React.ReactElement => {
+  const [placesService, setPlacesService] = useState<
+    google.maps.places.AutocompleteService | undefined
+  >(undefined);
+  const [sessionToken, setSessionToken] = useState<
+    google.maps.places.AutocompleteSessionToken | undefined
+  >(undefined);
+  const fetchSuggestions = useDebouncedCallback(
+    (
+      value: string,
+      cb: (options: OptionsType<OptionTypeBase>) => void
+    ): void => {
+      if (!placesService) return cb([]);
+      if (value.length < minLengthAutocomplete) return cb([]);
 
-    const autocompletionReq: AutocompletionRequest = { ...autocompletionRequest };
+      const autocompletionReq: AutocompletionRequest = {
+        ...autocompletionRequest,
+      };
 
-    placesService.getPlacePredictions(
-      autocompletionRequestBuilder(
-        autocompletionReq,
-        value,
-        withSessionToken && sessionToken,
-      ), (suggestions) => {
-        cb((suggestions || []).map(suggestion => ({ label: suggestion.description, value: suggestion })));
-      },
-    );
-  }, debounce);
+      placesService.getPlacePredictions(
+        autocompletionRequestBuilder(
+          autocompletionReq,
+          value,
+          withSessionToken && sessionToken
+        ),
+        (suggestions) => {
+          cb(
+            (suggestions || []).map((suggestion) => ({
+              label: suggestion.description,
+              value: suggestion,
+            }))
+          );
+        }
+      );
+    },
+    debounce
+  ).callback;
 
   const initializeService = () => {
-    if (!window.google) throw new Error('[react-google-places-autocomplete]: Google script not loaded');
-    if (!window.google.maps) throw new Error('[react-google-places-autocomplete]: Google maps script not loaded');
-    if (!window.google.maps.places) throw new Error('[react-google-places-autocomplete]: Google maps places script not loaded');
+    if (!window.google)
+      throw new Error(
+        "[react-google-places-autocomplete]: Google script not loaded"
+      );
+    if (!window.google.maps)
+      throw new Error(
+        "[react-google-places-autocomplete]: Google maps script not loaded"
+      );
+    if (!window.google.maps.places)
+      throw new Error(
+        "[react-google-places-autocomplete]: Google maps places script not loaded"
+      );
 
     setPlacesService(new window.google.maps.places.AutocompleteService());
     /* setServiceStatus(window.google.maps.places.PlacesServiceStatus.OK); */
     setSessionToken(new google.maps.places.AutocompleteSessionToken());
-  }
+  };
 
   useEffect(() => {
     const init = async () => {
@@ -52,7 +81,7 @@ const GooglePlacesAutocomplete: React.FC<GooglePlacesAutocompleteProps> = ({
       } catch (error) {
         onLoadFailed(error);
       }
-    }
+    };
 
     init();
   }, []);
@@ -65,6 +94,5 @@ const GooglePlacesAutocomplete: React.FC<GooglePlacesAutocompleteProps> = ({
     />
   );
 };
-
 
 export default GooglePlacesAutocomplete;
